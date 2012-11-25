5f72fa963015feaf9978368febb1b62e97967f0b
Sigurður Páll Guttormsson <sigurdurpg09@ru.is>
2012-11-25 23:59:09 +0000

    Grunnur

---
+++ src/Player.java
@@ -0,0 +1,30 @@
+//
+//
+//
+//  @ Project : Player
+//  @ File Name : Player.java
+//  @ Date : 25.11.2012
+//  @ Author : 
+//
+//
+
+
+
+
+public class Player {
+ public int NrOfMoves;
+ public boolean isTurn;
+ public String name;
+ public int Player;
+ public void setName() {
+ 
+ }
+ 
+ public String getName() {
+ 
+ }
+ 
+ public void changeTurn() {
+ 
+ }
+}
