ffbc1bba66371f8938409444af97b94a82e974bf
Sigurður Páll Guttormsson <sigurdurpg09@ru.is>
2012-11-25 23:59:09 +0000

    Grunnur

---
+++ src/TicTacToe.java
@@ -0,0 +1,38 @@
+//
+//
+//  Generated StarUML(tm) Java Add-In
+//
+//  @ Project : Untitled
+//  @ File Name : TicTacToe.java
+//  @ Date : 25.11.2012
+//  @ Author : 
+//
+//
+
+
+
+
+public final class TicTacToe {
+ public Box Board[9] = new Box[];
+ public int NrOfGames;
+ 
+ public void playTicTacToe() {
+ 
+ }
+ 
+ public void createPlayers() {
+ 
+ }
+ 
+ public int checkWin() {
+ 
+ }
+ 
+ public Player switchPl() {
+ 
+ }
+ 
+ public void main() {
+ 
+ }
+}
