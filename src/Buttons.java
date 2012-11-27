9a3c639e8cc07f88878c8483862e703803cbcb4e
Guðjón Eggert Steinþórsson <gudjons11@ru.is>
2012-11-27 06:22:31 +0000

    Buttons

--- src/Buttons.java
+++ src/Buttons.java
@@ -11,25 +11,31 @@ public class Buttons extends JFrame implements ActionListener{
         Frame frame = new Frame("Gaman");
         JButton playagain = new JButton("Play Again");
         frame.add(playagain);
+        playagain.setActionCommand("play");
         JButton cancel = new JButton("Cancel");
         frame.add(cancel);
-
+        cancel.setActionCommand("stop");
         frame.setLayout(new FlowLayout());
         frame.setSize(200, 100);
         frame.setVisible(true);
         cancel.addActionListener(this);
         playagain.addActionListener(this);
         //add(startButton);
-        //setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         //setVisible(true);
 
     }
     public void  actionPerformed(ActionEvent a) {
-           // for(int i = 0; true; i++) {
-             //       System.out.println(i);
+           if ("play".equals(a.getActionCommand()))  {
+            for(int i = 0; true; i++) {
+                System.out.println(i);
            // newgame.playTicTacToe();
-
             }
+        }   else {
+               setVisible(false);
+
+           }
+
     }
     public static void main(String[] args){
         new Buttons();
